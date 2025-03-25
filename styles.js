a68a2329390d78e45d94d35f2cbfca187cc26643
joshutik <utik2.4real@gmail.com>
2025-03-25 13:57:07 +0100

    made modification to the project page

--- styles.js
+++ styles.js
@@ -1,1 +1,8 @@
-let 
+document.addEventListener("DOMContentLoaded", function () {
+    var typed = new Typed("#typed-text", {
+        strings: ["Joshua Utik", "A Web Developer", "An Instructor", 'A Freelancer!'],
+        typeSpeed: 80,
+        backSpeed: 80,
+        loop: true
+    });
+});
